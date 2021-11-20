a928f7e0a33c5182c558453658c17cbc3eb008e4
Bhavy Rastogi <bhavyrastogi2002@gmail.com>
2021-11-20 16:22:35 +0530

    Updates

---
+++ precache-manifest.799e57ed601e071ac09a8efae2797aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc3368c7753b65156dda755ee797935",
+    "url": "/Catalogue/index.html"
+  },
+  {
+    "revision": "985cc40fb5f464ed11fd",
+    "url": "/Catalogue/static/css/main.69a310e6.chunk.css"
+  },
+  {
+    "revision": "cccfb548f5a651743664",
+    "url": "/Catalogue/static/js/2.af2e4b22.chunk.js"
+  },
+  {
+    "revision": "744f471492aed0fc25a03f669aabf39f",
+    "url": "/Catalogue/static/js/2.af2e4b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985cc40fb5f464ed11fd",
+    "url": "/Catalogue/static/js/main.3293b438.chunk.js"
+  },
+  {
+    "revision": "fbce4b5868798d85e0fc",
+    "url": "/Catalogue/static/js/runtime-main.53918b4a.js"
+  }
+]);
